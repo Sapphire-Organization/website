dcda84e0665719e64fb89cedd4a5c8c53bef088c
Victor Williams <vrwilliams719@gmail.com>
2023-11-16 02:04:36 -0500

    Add testing

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
---
+++ src/tests/app.test.tsx
@@ -0,0 +1,7 @@
+
+describe('App', () => {
+  it('should render', () => {
+    const test = true;
+    expect(test).toBeTruthy();
+  });
+});
